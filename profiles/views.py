3a4363a213cc412ee87d6f5ff2318f3095b2cea9
HassanKhan123 <hk260280@gmail.com>
2025-06-11 22:08:31 +0200

    serve uploaded files

--- profiles/views.py
+++ profiles/views.py
@@ -3,6 +3,8 @@
 from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponseRedirect
+from django.views.generic.edit import CreateView
+from django.views.generic import ListView
 
 from .forms import ProfileForm
 from .models import UserProfile
@@ -10,20 +12,14 @@ from .models import UserProfile
 # Create your views here.
 
 
-class CreateProfileView(View):
-    def get(self, request):
-        form = ProfileForm()
-        return render(request, "profiles/create_profile.html", {
-            "form": form
-        })
+class CreateProfileView(CreateView):
+    model = UserProfile
+    fields = "__all__"
+    template_name = "profiles/create_profile.html"
+    success_url = "/profiles/"
 
-    def post(self, request):
-        form = ProfileForm(request.POST, request.FILES)
-        if form.is_valid():
-            profile = UserProfile(image=request.FILES['user_image'])
-            profile.save()
-            return HttpResponseRedirect("/profiles/")
 
-        return render(request, "profiles/create_profile.html", {
-            "form": form
-        })
+class ProfilesView(ListView):
+    model = UserProfile
+    template_name = "profiles/user_profiles.html"
+    context_object_name = "profiles"
